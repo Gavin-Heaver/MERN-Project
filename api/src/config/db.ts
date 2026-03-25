e9c737ce7d413bf93beebf6da56ca88a94cb9892
prasaadjg <prasaadjg@hotmail.com>
2026-03-25 04:59:26 -0400

    simplified db connect

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -2,8 +2,6 @@ import mongoose from 'mongoose'
 import { config } from './env'
 
 export async function connectMongo(): Promise<void> {
-    await mongoose.connect(config.mongoUri, {
-        serverApi: { version: '1', strict: true, deprecationErrors: true }
-    })
+    await mongoose.connect(config.mongoUri)
     console.log('MongoDB connected')
 }
